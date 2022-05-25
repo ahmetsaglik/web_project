f922b94e3768cbb3f0f14359c15eebc1d4655e7c
Ahmet Sağlık <saglik46@gmail.com>
2022-05-26 00:54:28 +0300

    allowed_host append

--- web_project/settings.py
+++ web_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-zlcj4vfmtmz9cv@$*lalfip$4b5_2lxyo+dhnj3axvkg33+qrm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-app-template.herokuapp.com', '127.0.0.1']
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # During development only
